e08a3e44c472d2582e78ec6482895507c1519526
Chris Lanus <cqlanus@gmail.com>
2017-09-04 13:12:46 -0500

    user can login

--- components/Login.js
+++ components/Login.js
@@ -7,16 +7,43 @@ import Label from './Label';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import axios from 'axios';
+
 export default class Login extends Component {
   constructor() {
     super();
     this.state = {
-      text: ''
+      email: '',
+      password: ''
     }
   }
 
   press() {
+    this.requestLogin();
+  }
 
+  dummyPress() {
+
+  }
+
+  requestLogin = async () => {
+    try {
+      let response = await axios.post('http://192.168.1.67:8000/auth/login', this.state)
+      let result = await response.data;
+      console.log('result is:', result);
+    } catch(err) { console.error(err)}
+  }
+
+  handleChangeEmail(text) {
+    this.setState({
+      email: text.toLowerCase()
+    })
+  }
+
+  handleChangePass(text) {
+    this.setState({
+      password: text
+    })
   }
 
   render() {
@@ -26,13 +53,16 @@ export default class Login extends Component {
           <Button
             label="Forgot Login/Pass"
             styles={{button: styles.alignRight, label: styles.label}}
-            onPress={this.press.bind(this)} />
+            onPress={this.dummyPress.bind(this)} />
         </Container>
 
         <Container>
           <Label text="Email" />
           <TextInput
             style={styles.textInput}
+            value={this.state.email}
+            onChangeText={this.handleChangeEmail.bind(this)}
+            keyboardType="email-address"
           />
         </Container>
 
@@ -41,13 +71,15 @@ export default class Login extends Component {
           <TextInput
             secureTextEntry={true}
             style={styles.textInput}
+            value={this.state.password}
+            onChangeText={this.handleChangePass.bind(this)}
           />
         </Container>
 
         <Container>
           <Button
             styles={{button: styles.transparentButton}}
-            onPress={this.press.bind(this)}
+            onPress={this.dummyPress.bind(this)}
           >
             <View style={styles.inline}>
               <Icon name="google" size={30} color="#4885ed" />
@@ -70,7 +102,7 @@ export default class Login extends Component {
             <Button
               label='Cancel'
               styles={{label: styles.buttonBlackText}}
-              onPress={this.press.bind(this)} />
+              onPress={this.dummyPress.bind(this)} />
           </Container>
         </View>
 
