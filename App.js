4b1534d084b94f3e4030f2186b2439fcd3572ce2
Chris Lanus <cqlanus@gmail.com>
2017-09-20 20:54:37 -0500

    extracted navigation from App

--- App.js
+++ App.js
@@ -1,56 +1,10 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
-import  PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import {Tabs} from './config/router';
 
-import Login from './components/Login';
-import ProfileScreen from './components/ProfilePage';
-import MapScreen from './components/MapScreen';
-
-
-class App extends React.Component {
-  static navigationOptions = {
-    tabBarLabel: 'Login',
-  }
-
+class App extends Component {
   render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <View style={styles.container}>
-        <Login style={styles.login } navigate={navigate}/>
-      </View>
-    );
+    return <Tabs/>;
   }
 }
 
-App.propTypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func,
-  })
-};
-
-
-
-const SimpleApp = TabNavigator({
-  Home: { screen: ProfileScreen },
-  Map: { screen: MapScreen },
-  Notifications: { screen: App },
-}, {
-  tabBarOptions: {
-    activeTintColor: '#e91e63',
-  }
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    justifyContent: 'center',
-  },
-  login: {
-    marginBottom: 20
-  }
-});
-
-export default SimpleApp;
+export default App;
---
+++ config/router.js
@@ -0,0 +1,54 @@
+/* eslint react/prop-types: 0, react/display-name: 0, */
+
+import React from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Login from '../components/Login';
+import ProfileScreen from '../components/ProfilePage';
+import MapScreen from '../components/MapScreen';
+
+import ObservationPage from '../components/ObservationPage';
+
+export const ProfileStack = StackNavigator({
+  Profile: {
+    screen: ProfileScreen,
+    navigationOptions: {
+      title: 'Profile'
+    }
+  },
+  Observation: {
+    screen: ObservationPage,
+    navigationOptions: ({navigation}) => ({
+      title: `${navigation.state.params.name}`,
+    })
+  }
+});
+
+export const Tabs = TabNavigator({
+  Home: {
+    screen: ProfileStack,
+    navigationOptions: {
+      tabBarLabel: 'Me',
+      tabBarIcon: ({ tintColor }) => <Icon name="th-list" size={25} color={tintColor} />
+    }
+  },
+  Map: {
+    screen: MapScreen,
+    navigationOptions: {
+      tabBarLabel: 'Map',
+      tabBarIcon: ({ tintColor }) => <Icon name="map" size={25} color={tintColor} />
+    }
+  },
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      tabBarLabel: 'Login',
+      tabBarIcon: ({ tintColor }) => <Icon name="sign-in" size={25} color={tintColor} />
+    }
+  },
+}, {
+  tabBarOptions: {
+    activeTintColor: '#e91e63',
+  }
+});
